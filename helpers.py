405300bca8d1e823e524642793f636fa14407541
Shubham Aggarwal <shuaggar@microsoft.com>
2021-01-05 19:09:08 +0530

    initial

---
+++ helpers.py
@@ -0,0 +1,31 @@
+import requests
+from selectorlib import Extractor
+from bs4 import BeautifulSoup
+
+e_product = Extractor.from_yaml_file('selectors.yml')
+
+def scrape_search(url):  
+    r = requests.get(url)
+    # Simple check to check if page was blocked (Usually 503)
+    if r.status_code > 500:
+        return []
+    # Pass the HTML of the page and create 
+    soup = BeautifulSoup(r.text, "html.parser")
+    links = []
+    for link in soup.select('a.a-link-normal.a-text-normal'):
+        link_str = link.get('href')
+        if link_str:
+            links.append(link_str)
+    return links
+
+def scrape_product(url):
+    # Create an Extractor by reading from the YAML file
+    HEADERS = ({'User-Agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'})
+    r = requests.get(url, headers=HEADERS)
+    # Simple check to check if page was blocked (Usually 503)
+    if r.status_code > 500:
+        return None
+    # Pass the HTML of the page and create
+    return e_product.extract(r.text)
---
+++ scraper.py
@@ -0,0 +1,71 @@
+import multiprocessing
+from helpers import scrape_product, scrape_search
+import threading
+import sys
+import csv
+
+###########################################################################################
+# CONSTANTS
+THREAD_COUNT = multiprocessing.cpu_count() * 2
+PAGE_URL = "https://www.amazon.in/gp/bestsellers/hpc/1374621031/ref=zg_bs_nav_hp_3_1374620031"
+LOCK = threading.Lock()
+WRITE_CSV_HEADER = True
+MAX_PRODUCTS = 100
+###########################################################################################
+
+def append_to_csv(data):
+    with open('output.csv', 'a', encoding="utf-8") as f:
+        global WRITE_CSV_HEADER
+        w = csv.DictWriter(f, data.keys())
+        if WRITE_CSV_HEADER:
+            w.writeheader()
+            WRITE_CSV_HEADER = False
+        w.writerow(data)
+
+def product_list_fetcher(product_list):
+    for product_link in product_list:
+        data = scrape_product("https://www.amazon.in" + product_link)
+        if data and data['name']:
+            LOCK.acquire()
+            append_to_csv(data)
+            print("Fetch Success for - " + product_link)
+            LOCK.release()
+        else:
+            print(":( could not ftech product, try again")
+
+# fetch page results
+page_links = []
+i = 1
+while True:
+    next_url = PAGE_URL + "&pg=" + str(i)
+    page_data_links = scrape_search(next_url)
+    if page_data_links:
+        page_links += page_data_links
+    else:
+        break
+    if len(page_links) >= MAX_PRODUCTS:
+        page_links = page_links[:MAX_PRODUCTS]
+        break
+    i += 1
+
+if not page_links:
+    print("no links to crawl -- :( ")
+    sys.exit()
+
+###########################################################################################
+# FETCH PRODUCT LINKS USING MULTIPLE THREADS
+links_per_thread = len(page_links) // THREAD_COUNT
+threads = []
+curr_offset = 0
+while curr_offset < len(page_links):
+    links = page_links[curr_offset : curr_offset+links_per_thread]
+    curr_offset += links_per_thread
+    threads.append(threading.Thread(target=product_list_fetcher, args=(links,))) 
+
+# start threads
+for t in threads:
+    t.start()
+
+# join all
+for t in threads:
+    t.join()
